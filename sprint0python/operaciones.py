5a31adb960bce694c774faf88a0cada4767f521d
opilladov <opilladov24@fpcoruna.afundacion.org>
2024-10-02 12:27:29 +0200

    Completado ejercicio 1 (sumar, restar, dividir y multiplicar)

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,16 @@
+def suma(a,b):
+    return a+b
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    return num1 / num2
+
+print(suma(6,3))
+print(resta(6,3))
+print(multiplicacion(6,3))
+print(division(6,3))
