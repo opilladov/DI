27832d1f9db5ecde669e2e5ebf17e88cf6f4d01b
opilladov <opilladov24@fpcoruna.afundacion.org>
2024-10-09 09:13:05 +0200

    Añadida clase Monstruo.py

---
+++ sprint0python/Monstruo.py
@@ -0,0 +1,18 @@
+class Monstruo:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.ataque = ataque
+        self.defensa = defensa
+        self.salud = salud
+
+    def atacar(self, heroe):
+        print(f"El monstruo {self.nombre} ataca a {heroe.nombre}")
+        dano = self.ataque - (heroe.defensa + heroe.defensa_temporal)
+        if dano > 0
+            heroe.salud = heroe.salud - dano
+            print(f"El heroe {heroe.nombre} ha recibido {dano} puntos de daño")
+        else
+            print(f"El heroe ha bloqueado el ataque")
+
+    def esta_vivo(self):
+        return self.salud > 0
