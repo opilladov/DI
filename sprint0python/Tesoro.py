aa719bae8de3a2b06d77971d9a7c8af5203b9370
opilladov <opilladov24@fpcoruna.afundacion.org>
2024-10-09 09:23:30 +0200

    Añadida clase Tesoro.py

---
+++ sprint0python/Tesoro.py
@@ -0,0 +1,21 @@
+from random import random
+
+class Tesoro:
+    def __init__(self):
+        self.beneficios = ["aumento_ataque", "aumento_defensa", "restauracion_salud"]
+
+    def encontrar_tesoro(self, heroe):
+        beneficio = random.choice(self.beneficios)
+        print(f"El heroe ha encontrado un beneficio {beneficio}")
+
+        if beneficio == "aumento_ataque"
+            heroe.ataque = heroe.ataque + 5
+            print(f"El ataque de {heroe.nombre} aumenta a {heroe.ataque}")
+
+        elif beneficio == "aumento_defensa"
+            heroe.defensa = heroe.defensa + 5
+            print(f"La defensa del {heroe.nombre} aumenta a {heroe.defensa}")
+
+        else
+            heroe.salud = heroe.salud_maxima
+            print(f"La salud de {heroe.nombre} ha sido restaurada a {heroe.salud}")
