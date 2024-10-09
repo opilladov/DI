95a5fc03a593c47ae8b137b058086a6e1489fcd7
opilladov <opilladov24@fpcoruna.afundacion.org>
2024-10-09 09:49:31 +0200

    Añadida clase Mazmorra.py

---
+++ sprint0python/Mazmorra.py
@@ -0,0 +1,43 @@
+from Monstruo import Monstruo
+from Tesoro import Tesoro
+
+class Mazmorra:
+    def __init__(self, heroe):
+        self.heroe = heroe
+        self.monstruos = [Monstruo("Dragon", 12, 4, 80), Monstruo("Esbirro", 5, 10, 30), Monstruo("Gigante", 15, 3, 50)]
+        self.tesoro = Tesoro()
+
+    def jugar(self):
+        print("Heroe entra en la mazmorra")
+        for monstruo in self.monstruos:
+            print(f"Te has encontrado con {monstruo.nombre}")
+            self.enfrentar(monstruo)
+            if not self.heroe.esta_vivo():
+                print("El heroe ha sido derrotado en la mazmorra")
+                return
+            self.buscar_tesoro()
+        print(f"{self.heroe.nombre} ha derrotado a todos los monstruos y ha conquistado la mazmorra")
+
+    def enfrentar(self, enemigo):
+        while enemigo.esta_vivo() and self.heroe.esta_vivo():
+            print("¿Qué deseas hacer?")
+            print("1. Atacar")
+            print("2. Defender")
+            print("3. Curarse")
+            opcion = input("Selecciona una opción: ")
+            if opcion == "1":
+                self.heroe.atacar(enemigo)
+            elif opcion == "2":
+                self.heroe.defenderse()
+            elif opcion == "3":
+                self.heroe.curarse()
+            else:
+                print("Opción no válida")
+
+            if enemigo.esta_vivo():
+                enemigo.atacar(self.heroe)
+                self.heroe.reset_defensa()
+
+    def buscar_tesoro(self):
+        print("Buscando tesoro...")
+        self.tesoro.encontrar_tesoro(self.heroe)
