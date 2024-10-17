33c721790fb315eb454aed9b9a3f493026994c1e
opilladov <opilladov24@fpcoruna.afundacion.org>
2024-10-17 09:29:59 +0200

    Exercise 8 (frame) completed

---
+++ sprint1tkinter/frame.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+ventana = tk.Tk()
+ventana.title("Ventana de listbox")
+ventana.geometry("600x600")
+
+# Frame inferior
+frame_superior = tk.Frame(ventana)
+frame_superior.pack()
+
+label1 = tk.Label(frame_superior, text="Escribe algo")
+label1.pack()
+
+label2 = tk.Label(frame_superior, text="para tener contenido:")
+label2.pack()
+
+entry = tk.Entry(frame_superior)
+entry.pack()
+
+# Frame inferior
+frame_inferior = tk.Frame(ventana)
+frame_inferior.pack()
+
+def mostrar():
+    resultado = entry.get()
+    label_resultado.config(text=f"Resultado: {resultado}")
+
+def borrar():
+    entry.delete(0, tk.END)
+    label_resultado.config(text="")
+
+boton_mostrar = tk.Button(frame_inferior, text="Mostrar", command=mostrar)
+boton_mostrar.pack()
+
+boton_borrar = tk.Button(frame_inferior, text="Borrar", command=borrar)
+boton_borrar.pack()
+
+label_resultado = tk.Label(frame_inferior, text="")
+label_resultado.pack()
+
+ventana.mainloop()
