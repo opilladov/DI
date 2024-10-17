b77169af6e2f25580a5b5d497d94bfab85ea698a
opilladov <opilladov24@fpcoruna.afundacion.org>
2024-10-17 10:07:21 +0200

    Exercise 9 (menu) completed

---
+++ sprint1tkinter/menu.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from cProfile import label
+from tkinter import messagebox
+
+ventana = tk.Tk()
+ventana.title("Ventana de menu")
+ventana.geometry("600x600")
+
+def nueva():
+    messagebox.showinfo("Nuevo", "Abrir una nueva ventana")
+
+def salir():
+    ventana.destroy()
+
+menu = tk.Menu(ventana)
+ventana.config(menu=menu)
+
+archivo = tk.Menu(menu, tearoff=0)
+menu.add_cascade(label="Archivo", menu=archivo)
+archivo.add_command(label="Nuevo", command=nueva)
+archivo.add_separator()
+archivo.add_command(label="Salir", command=salir)
+
+def ayudar():
+    messagebox.showinfo("Ayuda acerca de", "Esto es informacion de ayuda")
+
+ayuda = tk.Menu(menu, tearoff=0)
+menu.add_cascade(label="Archivo", menu=ayuda)
+ayuda.add_command(label="Acerca de", command=ayudar)
+
+ventana.mainloop()
