1168f7204eee7b45df27ee7048a3e03f2d948fa8
opilladov <opilladov24@fpcoruna.afundacion.org>
2024-11-25 10:56:30 +0100

    Hotfix exercise 1 and completed exercise 2

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,28 +1,20 @@
 package com.example.mycatalog;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CatalogActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_catalog);
-
-        Button navigateButton = findViewById(R.id.navegarDetalle);
-
-        navigateButton.setOnClickListener(view -> {
-            Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
-            startActivity(intent);
-        });
-
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_detail);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
